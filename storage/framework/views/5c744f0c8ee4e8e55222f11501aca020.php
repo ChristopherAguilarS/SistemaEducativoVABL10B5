e62c4ce50b0107f1b7ef9119190c9e8c02742c82
GUSTAVO <tavo8040@gmail.com>
2024-02-29 12:01:58 -0500

    dwdw

--- storage/framework/views/5c744f0c8ee4e8e55222f11501aca020.php
+++
@@ -1,5 +0,0 @@
-
-<body>
-
-<!-- <body data-layout="horizontal"> -->
-<?php /**PATH C:\Users\tavo_\OneDrive\Escritorio\Proyectos\SistemaEducativoVABL10B5\resources\views/layouts/body.blade.php ENDPATH**/ ?>
