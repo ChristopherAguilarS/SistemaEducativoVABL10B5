c9bb6d6e2cb1269f7f70c6703d461a66da46dd4a
GUSTAVO <tavo8040@gmail.com>
2024-02-29 12:18:34 -0500

    dw

--- app/Livewire/configuracion/Contable/Cuentas/Filtro.php
+++ app/Livewire/configuracion/Contable/Cuentas/Filtro.php
@@ -4,8 +4,7 @@ namespace App\Livewire\Configuracion\Contable\Cuentas;
 
 use Livewire\Component;
 
-class Filtro extends Component
-{
+class Filtro extends Component {
     public function render()
     {
         return view('livewire.configuracion.contable.cuentas.filtro');
